475569ad7821c64c4c82ba809ec3cbbc820397e4
Nicholas Gentz <123503564+nGentz@users.noreply.github.com>
2023-10-16 22:38:53 -0400

    Add files via upload

--- dash_app_mta.py
+++ dash_app_mta.py
@@ -3,29 +3,11 @@ import dash
 from dash import dcc, html, Input, Output
 import plotly.express as px
 
-# Read csv file to pd dataframe
-df = pd.read_csv('MTA_Subway_Hourly_Ridership__Beginning_February_2022.csv')
-
-by_station_df = df.groupby(['station_complex_id', 'station_complex', 'borough', 'latitude', 'longitude'])['ridership'].sum().reset_index()
-
-by_station_df['ridership_per_day'] = (by_station_df['ridership'] / 533).round(0)
+# read csv
+df = pd.read_csv('/Users/nickgentz/Downloads/dash_app/by_station_df.csv')
 
 # Specify your Mapbox access token (replace 'YOUR_MAPBOX_TOKEN' with your actual token)
 mapbox_token = 'pk.eyJ1Ijoibmlja2dlbnR6IiwiYSI6ImNsMnE4MXBqYTFodWozamt6Y3UzaDN2bm8ifQ.sudv6WDuPydt3LtqMLJZlw'
-
-by_station_df['scaled_marker_size'] = (by_station_df['ridership_per_day'] / by_station_df['ridership_per_day'].max() * 9).round(4)
-
-by_station_df['station_complex'] = by_station_df['station_complex'].str.replace(r'\([^()]*\)', '')
-
-by_station_df['locked_marker_size'] = 1
-
-bins = [161.55, 10000, 50000, 75000, 135546]
-bin_labels = [
-    'Low (161 - 10000)',
-    'Medium (10,000 - 50,000)',
-    'High (50,000 - 75,000)',
-    'Very High (75,000+)'
-]
 
 bin_colors = {
     'Low (161 - 10000)': 'rgb(255, 195, 0)',
@@ -33,10 +15,6 @@ bin_colors = {
     'High (50,000 - 75,000)': 'rgb(199, 0, 57)',
     'Very High (75,000+)': 'rgb(88, 24, 69)'
 }
-
-by_station_df['ridership_bin'] = pd.cut(by_station_df['ridership_per_day'], bins=bins, labels=bin_labels)
-
-by_station_df = by_station_df.sort_values(by=['ridership_per_day'], ascending=False).reset_index(drop=True)
 
 app = dash.Dash(__name__)
 server = app.server
@@ -69,15 +47,15 @@ def update_marker_size(selected_values):
     )
 
     fig = px.scatter_mapbox(
-        by_station_df,
+        df,
         lat='latitude',
         lon='longitude',
         color='ridership_bin',
         hover_name='station_complex',
         custom_data=[
-            by_station_df['ridership_per_day'],
-            by_station_df['ridership'],
-            by_station_df['station_complex']
+            df['ridership_per_day'],
+            df['ridership'],
+            df['station_complex']
         ],
         size='scaled_marker_size' if use_scaled_marker_size else None,
         color_discrete_map=bin_colors
